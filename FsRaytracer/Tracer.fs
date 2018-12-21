a8b56fa898a83615322f853c9a8678da5ccd3134
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2018-12-21 19:35:56 +0100

    Sphere with normals

--- FsRaytracer/Tracer.fs
+++ FsRaytracer/Tracer.fs
@@ -2,6 +2,7 @@
 
 open TracerData
 open System.Numerics
+open System
 
 type RenderSurface = {
     height: int
@@ -17,21 +18,30 @@ let sphereRadius = 0.5f
 
 let colorRed = vec3 1.0f 0.0f 0.0f
 
+let inline sqrt (f: float32) = float32 (Math.Sqrt (float f))
+
 let hitSphere (center: Vector3) (radius: float32) (ray: Ray) =
     let oc = ray.origin - center
     let a = dotP ray.direction ray.direction
     let b = 2.0f * (dotP oc ray.direction)
     let c = (dotP oc oc) - (radius * radius)
     let discriminant = b * b - 4.0f * a * c
-    discriminant > 0.0f
+    if discriminant < 0.0f then
+        None
+    else
+        Some ((-b - (sqrt discriminant)) / (2.0f * a))
+    
 
 let color (ray: Ray) =
-    if (hitSphere sphereCenter sphereRadius ray) then
-        colorRed
-    else
-        let normDirection = Vector3.Normalize ray.direction
-        let t = 0.5f * (normDirection.Y + 1.0f)
-        (oneVector * (1.0f - t)) + (t * colorVector)
+    match (hitSphere sphereCenter sphereRadius ray) with
+        | Some hit ->
+            if hit < 0.0f then invalidOp "Hit may not be negative"
+            let n = (norm (calculatePosition ray hit) - (vec3 0.0f 0.0f -1.0f))
+            0.5f * (n + oneVector)
+        | None ->
+            let normDirection = Vector3.Normalize ray.direction
+            let t = 0.5f * (normDirection.Y + 1.0f)
+            (oneVector * (1.0f - t)) + (t * colorVector)
 
 let trace (surface: RenderSurface) =
     let { height = height; width = width; setColor = setColor } = surface
--- FsRaytracer/TracerData.fs
+++ FsRaytracer/TracerData.fs
@@ -16,3 +16,5 @@ let inline vec3 (x: float32) (y: float32) (z: float32) = Vector3(x, y, z)
 let inline mul (s: float32) (vec: Vector3) = Vector3.Multiply(vec, s)
 
 let inline dotP (vec1: Vector3) (vec2: Vector3) = Vector3.Dot(vec1, vec2)
+
+let inline norm (vec: Vector3) = Vector3.Normalize(vec)
