e8b3a68dd98ea19139944de082be008770d0d62b
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2018-12-23 14:31:33 +0100

    Fix sphere collision detection

--- FsRaytracer/TracerData.fs
+++ FsRaytracer/TracerData.fs
@@ -36,28 +36,23 @@ let inline norm (vec: Vector3) = Vector3.Normalize(vec)
 
 let rec hit (ray: Ray) (tmin: float32) (tmax: float32) (body: SceneObject) =
     match body with
-        | Sphere sphere ->
+        | Sphere { center = center; radius = radius } ->
             let isHit t =
                 if t < tmax && t > tmin then
                     let pos = (calculatePosition ray t)
-                    Some { parameter = t; position = pos; normal = (pos - sphere.center) / sphere.radius }
+                    Some { parameter = t; position = pos; normal = (pos - center) / radius }
                 else
-                None
-            let oc = ray.origin - sphere.center
+                    None
+
+            let oc = ray.origin - center
             let a = dotP ray.direction ray.direction
-            let b = dotP oc ray.direction
-            let c = (dotP oc oc) - (sphere.radius * sphere.radius)
-            let discriminant = (b * b) - (a * c)
-            if discriminant > 0.0f then
-                let t1 = (-b - (sqrt (b * b) - (a * c))) / a
-                match (isHit t1) with
-                    | None ->
-                        let t2 = (-b + (sqrt (b * b) - (a * c))) / a
-                        isHit t2
-                    | Some hit ->
-                        Some hit
-            else
+            let b = 2.0f * (dotP oc ray.direction)
+            let c = (dotP oc oc) - (radius * radius)
+            let discriminant = b * b - 4.0f * a * c
+            if discriminant < 0.0f then
                 None
+            else
+                isHit ((-b - (sqrt discriminant)) / (2.0f * a))
 
         | Objects objects ->
             let hitFound, _ = Seq.fold (fun (currentHit, closest) o ->
