68723710db9834bbd991fa108f19ce195bfc22f2
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2019-01-04 18:40:38 +0100

    Random colorful spheres

--- RaytracerCLI/Program.fs
+++ RaytracerCLI/Program.fs
@@ -17,17 +17,24 @@ let imageSurface (image: Image<Rgba32>) =
         image.Item (x, image.Height - 1 - y) <- (toRgba32 col)
     { height = image.Height; width = image.Width; setColor = setColor }
 
+let randomScene =
+    let ground = sphere (vec3 0.0f -1000.0f 0.0f) 1000.0f (lambertian (vec3 0.5f 0.5f 0.5f) rng)
+    let rp () = rng () - 0.5f
+    let randomSpheres = [0..100] |> List.map (fun _ ->
+        let y = (rng () * 0.5f) + 0.1f
+        let center = (vec3 (rp ()) (y / 10.0f) (rp ())) * 10.0f
+        sphere center y (lambertian (vec3 (rng ()) (rng ()) (rng ())) rng)
+    )
+    ground :: randomSpheres
+    
 [<EntryPoint>]
 let main argv =
     let width = 200
     let height = 100
     let ar = (float32 width) / (float32 height)
-    let camera = fromOrigin 60.0f ar 1.0f 2.0f
+    let camera = createCamera (vec3 0.0f 2.0f -4.0f) (vec3 0.0f 0.0f -1.0f) (vec3 0.0f 1.0f 0.0f) 60.0f ar 0.0f 2.0f
     let r = pi / 4.0f
-    let world = group [
-        sphere (vec3 -r 0.0f -1.0f) r (lambertian (vec3 0.0f 0.0f 1.0f) rng);
-        sphere (vec3 r 0.0f -1.0f) r (lambertian (vec3 0.0f 1.0f 0.0f) rng)
-    ]
+    let world = group randomScene
     let image = new Image<Rgba32>(width, height)
     let time = traceParallel camera world defaultSettings (imageSurface image)
     image.Save("output.png")
